3d87d59aaf97ddc9f3164312486e8b4f297bce17
Callumwils00 <90720791+Callumwils00@users.noreply.github.com>
2022-02-20 15:11:57 +0000

    Add files via upload

---
+++ Covid-19 Shiny.R
@@ -0,0 +1,60 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(shiny)
+library(remotes)
+
+
+Data <- read_csv(##paste in data your directory )
+
+Data$Date <- as.Date(Data$date)
+
+
+ui <- fluidPage(
+  titlePanel("Covid-19 cases"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "Country",label = h3("Select Countries to Compare"),
+              choices = c(unique(Data$location)),
+              multiple = TRUE),
+  
+  selectInput(inputId = "Variant", label = h3("Select a Variant"),
+              choices = c(unique(Data$variant)),
+              multiple = FALSE)),
+  
+  mainPanel(
+    plotOutput("line_graph")
+  )
+  )
+)
+  
+
+
+
+Server <- function(input, output){
+  
+  Country <- c(Data$location) 
+  Date <- c(Data$Date)
+  Cases <- c(Data$num_sequences)
+  Variant <- c(Data$variant)
+  df <- data.frame(Country, Date, Cases, Variant)
+  
+ 
+  ## This section allows for reactive subsetting 
+  df_subset <- reactive({
+    a <- subset(df, Country %in% input$Country
+                & Variant %in% input$Variant, 
+                )
+    return(a)
+  })
+  
+  output$line_graph <- renderPlot({
+    ggplot(df_subset(), aes(x = Date, y = Cases,
+                         color = Country)) +
+      geom_line(size = 1)
+      
+})
+}
+
+shinyApp(ui = ui, server = Server)
+
